4148521d8b4434310bb542bccdf7a76cad1b096b
EdvinasPa <epedvis9@gmail.com>
2023-07-18 18:39:28 +0300

    Added project

---
+++ src/test/java/codeacademy/API/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package codeacademy.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
